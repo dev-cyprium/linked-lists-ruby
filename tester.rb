4b41333a3c3146013f7509bc836ff708f71c39df
destroyergm <stefan_vg@hotmail.com>
2016-06-18 23:46:20 +0200

    Dir and class outline

---
+++ tester.rb
@@ -0,0 +1,21 @@
+require_relative "linked_lists"
+
+class LinkedList
+	class Node
+		def initialize value
+			@value = value
+			@next = nil
+		end
+	end
+
+	def initialize
+		@head = nil
+		@tail = nil
+	end
+
+	
+
+	def to_s
+
+	end
+end
